5c284d4fe0e59c0cd9e4359d8e4d621e82708091
karemAlba <krmd91@gmail.com>
2016-10-19 17:45:12 +0000

    Ejercicios terminados Dom2

--- Dom2/bicis-locas-lvl2/js/main.js
+++ Dom2/bicis-locas-lvl2/js/main.js
@@ -1,41 +1,60 @@
 function validateForm(){
-	var name = document.getElementById('name').value ;
-	var lastname = document.getElementById('lastname').value;
-	var email = document.getElementById('input-email').value;
-	var password = document.getElementById('input-password').value;
+	var regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/; //regex Inidica que es una expresion regular.
+	var regexNombre = /^[A-Z][a-z]*$/;
+	var name = document.getElementById("name").value
+	var lastname = document.getElementById ("lastname").value;
+	var mail = document.getElementById ("input-email").value; 
+	var password = document.getElementById("input-password").value;
+	var bici = document.getElementById("selecciona").selectedIndex;
+	
 
-	if( name == "" ) {
-  		document.getElementById('nombre').style.display = 'inline';
-  		return false;
-	}else {
-		document.getElementById('nombre').style.display = 'none';
-	}
-	if(!/^[a-zA-Z]*$/g.test(document.getElementById('name').value)){
-		document.getElementById('mayusculas').style.display = 'inline';
-		return false;
-	}else {
-		document.getElementById('mayusculas').style.display = 'none';
+
+
+	if(name == "" || lastname == "" || mail == "" || password == "" ){
+		alert("Los campos no tienen que estar vacios");
+		return;
 	}
 
-	if( lastname == "" ) {
-  		alert("Apellido no puede estar vacio");
-  		return false;
-	}else if(!/^[a-zA-Z]*$/g.test(document.getElementById('lastname').value)){
-		alert("El Apellido tiene que tener solo letras");
-		return false;
+
+	var boxes = document.getElementsByClassName("input-box") 
+	for (var i = 0; i < boxes.length; i++){ 
+		console.log(boxes[i].lastChild.nodeName); 
+		if (boxes[i].lastChild.nodeName === "SPAN")  //Elimina al hijo del padre.
+		{
+			boxes[i].removeChild(boxes[i].lastChild);
+		}
 	}
-	if( email == "" ) {
-  		alert("Email no puede estar vacio");
-  		return false;
+	
+	
+	if (!regexNombre.test(name)){ 
+		var boxname = document.getElementById("boxname");
+		var span = document.createElement("span");
+		span.appendChild(document.createTextNode("La primer letra de tu nombre debe ser mayúscula"));
+		boxname.appendChild(span);
+		return;
 	}
-	if( password == "" ) {
-  		alert("Contraseña no puede estar vacio");
-  		return false;
-	}else if(password.length < 6){
-		alert("La contraseña debe conterner al menos 6 caracteres");
-		return false;
-	}else if (password == "password" || password == 123456 || password == 0987654){
-		alert("Contraseña no valida");
-		return false;
+	if (!regexNombre.test(lastname)){
+		var boxlastname = document.getElementById("boxlastname");
+		var span = document.createElement("span");
+		span.appendChild(document.createTextNode("La primer letra de tu apellido debe ser mayúscula"));
+		boxlastname.appendChild(span);
+	}
+	if(!regexEmail.test(mail)){
+		var boxmail = document.getElementById("boxmail");
+		var span = document.createElement("span");
+		span.appendChild(document.createTextNode("Escribe de nuevo tu mail"));
+		boxmail.appendChild(span);
+	}
+	if(password.length < 6){
+		var boxpswd = document.getElementById("boxpswd");
+		var span = document.createElement("span");
+		span.appendChild(document.createTextNode("Tu contraseña debe contener 6 caracteres"));
+		boxpswd.appendChild(span);
+	}
+	if(bici == 0){
+		var boxbici = document.getElementById("boxbici");
+		var span = document.createElement("span");
+		span.appendChild(document.createTextNode("Selecciona un elemento de la lista"));
+		boxbici.appendChild(span);
 	}
 }
